5ce2b58dafb19898050851fc503ddf05d7767fdf
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-25 16:14:23 -0600

    feat: refactored user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,6 +24,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:password, :password_confirmation, :username, :email)
+    params.permit(:password, :password_confirmation, :username, :email)
   end
 end
