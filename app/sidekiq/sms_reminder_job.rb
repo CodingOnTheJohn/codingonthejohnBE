493fc0f32ebad8fb4e9a8e34b034214b6daec2fe
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-29 22:41:28 -0500

    WIP: Model Method Text Pref

--- app/sidekiq/sms_reminder_job.rb
+++ app/sidekiq/sms_reminder_job.rb
@@ -1,8 +1,8 @@
 class SmsReminderJob
   include Sidekiq::Job
 
-  def perform(users)
-    users.each do |user|
+  def perform
+    User.text_opt_in.each do |user|
       SmsService.send_sms(user.phone_number, "Hey! Don't forget to get your coding practice today!")
     end
   end
