97b550bb49bedeaf228973e31a37ed7af62c4340
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-31 11:52:17 -0500

    Bug Fix

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       resources :users, only: %i[new create show]
 
       get '/login', to: "users#login"
-      post '/auth/github', to: "users#github"
+      get '/auth/github', to: "users#github"
       get '/auth/github/callback', to: "users#github_callback"
       
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(username: 'gbowman', email: 'garrett.r.bowman@gmail.com', password: 'password', phone_number: '+1302-563-5882', text_preference: true)
